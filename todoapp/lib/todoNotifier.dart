e53ff91d8a5653ad64494365d2e1c78d8b462d07
トム・クルーズ <ryu1421magic@icloud.com>
2024-05-04 12:51:51 +0900

    create notifier

--- todoapp/lib/todoNotifier.dart
+++ todoapp/lib/todoNotifier.dart
@@ -1,1 +1,17 @@
+import 'dart:async';
 
+@riverpod
+class TodoNotifier extends _$TodoNotifier {
+  @override
+  Stream<String> build() {
+    // １秒ごとに通知を受け取るstream
+    final controller = StreamController<String>();
+    const sec1 = Duration(seconds: 1);
+    final timer = Timer.periodic(sec1, (timer) {
+      controller.add('メッセージが${timer.tick}件届きました');
+    });
+    return controller.stream;
+  }
+
+  // データ変更のメソッドたちを記述
+}
